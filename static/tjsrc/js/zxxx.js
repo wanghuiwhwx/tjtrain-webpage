1d560e93669c3fe8e4df4bb837b2938948b91a9d
GuoYang <guoyang@taihe.com>
2018-09-29 18:20:33 +0800

    change

--- static/tjsrc/js/zxxx.js
+++ static/tjsrc/js/zxxx.js
@@ -2,4 +2,24 @@ $(function() {
   $(".tag-box").on("click","li",function() {
     $(this).addClass("on").siblings("li").removeClass("on")
   })
+  $.ajax({
+    type: "get",
+    url: "../../demoJson/json111.json",
+    data: "",
+    dataType: "json",
+    success: function (res) {
+      console.log(res)
+      var arr = []
+      var html = ""
+      for(var i = 0;i<res.data.list.length;i++) {
+        if(res.data.list[i].parentId == 0) {
+          arr.push(res.data.list[i])
+        }
+      }
+      for(var j=0;j<arr.length;j++) {
+        html+= '<li>'+arr[j].name+'</li>'
+      }
+      $(".tag-box").append(html);
+    }
+  });
 })

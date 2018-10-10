dc684eed12cd8962c8366e52abaab0c90ccbeece
GuoYang <guoyang@taihe.com>
2018-10-10 19:30:00 +0800

    在线练习

--- static/tjsrc/js/zxlx.js
+++ static/tjsrc/js/zxlx.js
@@ -2,22 +2,33 @@ $(function (param) {
   $(".selectpicker").selectpicker()
   $(".selectpicker2").selectpicker()
   $(".selectpicker3").selectpicker()
-  $(".widget-sider").sider({
-    min:0,
-    max:100 //拖动滑块的最大及最小值
-  })
+  // $(".widget-sider").sider({
+  //   min:0,
+  //   max:100 //拖动滑块的最大及最小值
+  // })
+  $( "#slider-range" ).slider({
+    range: true,
+    min: 0,
+    max: 9,
+    values: [ 0, 9 ],
+    slide: function( event, ui ) {
+      $( "#amount" ).val( ui.values[ 0 ] + " - " + ui.values[ 1 ] );
+    }
+  });
+  $( "#amount" ).val( $( "#slider-range" ).slider( "values", 0 ) +" - " + $( "#slider-range" ).slider( "values", 1 ) );
   var intervalHandler;
   var sjData;
   var count=0;
   // 生成试卷
   $(".scsj-btn").on("click",function (param) { 
     var type = $(".selectpicker").val() //业务分类value值
-    var ny = $(".form-control").val() //难易程度value值
+    var ny_bottom = $( "#slider-range" ).slider( "values", 0 ) //易程度value值
+    var ny_top = $( "#slider-range" ).slider( "values", 1 ) //难程度value值
     var num = $(".nums").val()  //  题目数量vaule值
-    console.log(num)
+    console.log(ny_bottom,ny_top)
     $.ajax({
       type: "get",
-      url: "../../demoJson/json142.json",
+      url: "../../demoJson/json143.json",
       data: "",
       dataType: "json",
       success: function (res) {
@@ -36,12 +47,20 @@ $(function (param) {
               html += '<li>'
                 +'<h3 class="title"><span>'+(i*1+1)+'、</span>【多选题】'+res.data.tmList[i].tmDesc+'【'+res.data.tmList[i].tmScore+'分】</h3>'
                 +tplCheckbox(res.data.tmList[i].tmItems,res.data.tmList[i].tmId)
+                +'<div class="tm-result" id="tm-result-'+res.data.tmList[i].tmId+'"><span class="zqda"></span><span class="ndda"></span></div>'
               +'</li>'
             }else if(res.data.tmList[i].tmType == "单选题") {
               html += '<li>'
               +'<h3 class="title"><span>'+(i*1+1)+'、</span>【单选题】'+res.data.tmList[i].tmDesc+'【'+res.data.tmList[i].tmScore+'分】</h3>'
               +'</li>'
               +tplRadio(res.data.tmList[i].tmItems,res.data.tmList[i].tmId)
+              +'<div class="tm-result" id="tm-result-'+res.data.tmList[i].tmId+'"><span class="zqda"></span><span class="ndda"></span></div>'
+            }else if(res.data.tmList[i].tmType == "判断题") {
+              html += '<li>'
+              +'<h3 class="title"><span>'+(i*1+1)+'、</span>【判断题】'+res.data.tmList[i].tmDesc+'【'+res.data.tmList[i].tmScore+'分】</h3>'
+              +'</li>'
+              +tplRadio(res.data.tmList[i].tmItems,res.data.tmList[i].tmId)
+              +'<div class="tm-result" id="tm-result-'+res.data.tmList[i].tmId+'"><span class="zqda"></span><span class="ndda"></span></div>'
             }
             
           }
@@ -66,14 +85,14 @@ $(function (param) {
    function tplCheckbox(res,tmid) {
      var html = ""
      for(var i = 0;i<res.length;i++) {
-      html += '<p><span><input type="checkbox" name="'+tmid+'"></span>'+res[i].itemSign+'、'+res[i].itemDesc+'</p>'
+      html += '<p><span><input type="checkbox" name="'+tmid+'" value="'+res[i].itemSign+'"></span>'+res[i].itemSign+'、'+res[i].itemDesc+'</p>'
      }   
      return html
    }
    function tplRadio(res,tmid) {
     var html = ""
      for(var i = 0;i<res.length;i++) {
-      html += '<p><span><input type="radio" name="'+tmid+'"></span>'+res[i].itemSign+'、'+res[i].itemDesc+'</p>'
+      html += '<p><span><input type="radio" name="'+tmid+'" value="'+res[i].itemSign+'"></span>'+res[i].itemSign+'、'+res[i].itemDesc+'</p>'
      }   
      return html
    }
@@ -83,10 +102,60 @@ $(function (param) {
       clearInterval(intervalHandler)
       count = 0
       $(this).addClass('disable')
+      $("input[type='checkbox']").attr("disabled", "disabled");
+      $("input[type='radio']").attr("disabled", "disabled");
       console.log(sjData)
+      //获取参数
+      var _data={};
+      _data.id=sjData.id;
+      _data.beginTime=sjData.beginTime;
+      _data.endTime=new Date();
+      _data.resultType=1;
+      var allAnswer={};
+      
       var tmList = sjData.tmList
       for(var i = 0;i<tmList.length;i++) {
+        if (tmList[i].tmType=="判断题" || tmList[i].tmType=="单选题") {
+          allAnswer[tmList[i].tmId]=$('input[name="'+tmList[i].tmId+'"]:checked').val();
+        }else {
+          var answer="";
+          allAnswer[tmList[i].tmId]=$('input[name="'+tmList[i].tmId+'"]:checked').val();
+          $('input[name="'+tmList[i].tmId+'"]:checked').each(function(){
+            answer+=$(this).val()+",";
+          });
+          answer=answer.substring(0,answer.length-1)
+          allAnswer[tmList[i].tmId]=answer
+        }
         
+        // var oneTm=tmList[i]
+        // $("#tm-result-"+oneTm.tmId).find(".zqda").html("正确答案："+oneTm.tmAnswer)
+        // $("#tm-result-"+oneTm.tmId).find(".ndda").html("您的答案："+(!allAnswer[oneTm.tmId] ? "未答" : allAnswer[oneTm.tmId]))
       }
+      _data.answers=allAnswer
+      $.ajax({
+        type: "get",
+        url: "../../demoJson/json142.json",
+        data: "",
+        dataType: "json",
+        success:function(res) {
+          console.log(res)
+          var oktm = []
+          var total = 0
+          for(var i = 0;i<res.data.tmList.length;i++) {
+            var oneTm=res.data.tmList[i]
+            $("#tm-result-"+oneTm.tmId).find(".zqda").html("正确答案："+oneTm.tmAnswer)
+            $("#tm-result-"+oneTm.tmId).find(".ndda").html("您的答案："+(!allAnswer[oneTm.tmId] ? "未答" : allAnswer[oneTm.tmId]))
+            if(oneTm.tmAnswer == allAnswer[oneTm.tmId]) {
+              oktm.push(oneTm)
+            }
+          }
+          for(var i = 0;i<oktm.length;i++) {
+            total += oktm[i].tmScore
+          }
+          $(".dd").html(oktm.length)  //答对题数
+          $(".df").html(total) // 得分
+        }
+      })
+      
     })
  })
